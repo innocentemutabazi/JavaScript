b4d6f4deb2b3ff1cc5c125c6e2f711f086059a1a
innocentemutabazi <innocentemutabazi@gmail.com>
2024-02-26 12:27:18 +0200

    Variable and Datatype

---
+++ day1/variables.js
@@ -0,0 +1,9 @@
+const networth=1000;
+const myName='Innocente';
+const amIAMillionaire = false;
+const array =["Innocente", 1000, false];
+const shoppingList= ["Bentley","Porche","Mercedes", "Audi"]
+console.log ("My name is " + myName + " and my current networth is " + networth + "$");
+console.log (`my name is ${myName} and my current networth is ${networth}$`);
+console.log (array);
+console.log (shoppingList);
