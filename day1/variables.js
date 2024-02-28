0e4de3810c7a5fd4bfd42fe30ea8989a4393ac4c
innocentemutabazi <innocentemutabazi@gmail.com>
2024-02-28 10:28:31 +0200

    day 2 folder

--- day1/variables.js
+++ day1/variables.js
@@ -1,9 +1,24 @@
+
+// variable 
 const networth=1000;
 const myName='Innocente';
 const amIAMillionaire = false;
+//array
 const array =["Innocente", 1000, false];
 const shoppingList= ["Bentley","Porche","Mercedes", "Audi"]
 console.log ("My name is " + myName + " and my current networth is " + networth + "$");
 console.log (`my name is ${myName} and my current networth is ${networth}$`);
 console.log (array);
 console.log (shoppingList);
+
+//function 
+function myCar(){
+    let myCar = 'Bentley'
+    console.log (myCar);
+}
+
+
+
+//object
+
+
